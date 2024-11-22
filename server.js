ef1c97c4f4758507ec822533ba46626792a9a4ea
famous-kan <Kanyakorn_fame@hotmail.com>
2024-11-22 15:40:25 +0700

    origin

--- server.js
+++ server.js
@@ -13,7 +13,11 @@ const {authenticate} = require('./middlewares/authenticate')
 
 
 
-app.use(cors())
+app.use(cors(
+    {
+        origin : "*"
+    }
+))
 app.use(express.json())
 app.use('/auth', authRoute)
 app.use('/product',productRoute)
